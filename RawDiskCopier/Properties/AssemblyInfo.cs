5472cdf9b4b6020163ee21e617d3d21b851a0af5
Tal Aloni <tal.aloni.il@gmail.com>
2019-10-31 22:56:47 +0200

    Raw Disk Copier v1.0.4

--- RawDiskCopier/Properties/AssemblyInfo.cs
+++ RawDiskCopier/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tal Aloni")]
 [assembly: AssemblyProduct("Raw Disk Copier")]
-[assembly: AssemblyCopyright("Copyright © Tal Aloni 2017-2018")]
+[assembly: AssemblyCopyright("Copyright © Tal Aloni 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
