3faa55220df5a09236485a4809adc431b58636ad
TalAloni <tal.aloni.il@gmail.com>
2020-05-26 17:55:47 +0300

    UI: Disabled automatic scaling

--- RawDiskCopier/MainForm.Designer.cs
+++ RawDiskCopier/MainForm.Designer.cs
@@ -266,7 +266,7 @@ namespace RawDiskCopier
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(568, 183);
             this.Controls.Add(this.groupOptions);
             this.Controls.Add(this.lblTargetDiskSerialNumber);
