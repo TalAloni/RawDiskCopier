42f1e207cf3058bcc22976c18f67d1680fd7dab1
Tal Aloni <tal.aloni.il@gmail.com>
2024-02-17 14:13:59 +0200

    Removed Visual Studio 2005 support

--- RawDiskCopier/ILMerge/ILMerge.VS2005.bat
+++
@@ -1,6 +0,0 @@
-set binaryPath=%CD%\..\bin\Release
-set outputPath=%CD%\..\bin\ILMerge
-IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
-IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
-IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
-%ilmergePath%\ilmerge /ndebug /target:winexe /out:"%outputPath%\RawDiskCopier.exe" "%binaryPath%\RawDiskCopier.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\DiskAccessLibrary.FileSystems.Abstractions.dll" "%binaryPath%\Utilities.dll"
